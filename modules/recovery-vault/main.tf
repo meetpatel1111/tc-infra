1ca1b5cedbfb76666c9b1afc03e475159c1b9e42
Meetkumar Patel <pmeet464@gmail.com>
2026-02-10 14:22:18 +0530

    Fix VM workload backup policy schema

--- modules/recovery-vault/main.tf
+++ modules/recovery-vault/main.tf
@@ -68,45 +68,34 @@ resource "azurerm_backup_policy_vm_workload" "sql" {
   resource_group_name = var.resource_group_name
   recovery_vault_name = azurerm_recovery_services_vault.this.name
 
-  backup_management_type = "AzureWorkload"
-  workload_type          = "SQLDataBase"
+  workload_type = "SQLDataBase"
 
   settings {
     time_zone = var.sql_workload_policy.timezone
   }
 
-  sub_protection_policy {
+  protection_policy {
     policy_type = "Full"
 
-    schedule_policy {
-      schedule_policy_type   = "SimpleSchedulePolicy"
-      schedule_run_frequency = "Daily"
-      schedule_run_times     = [var.sql_workload_policy.full_backup_time]
+    backup {
+      frequency = "Daily"
+      time      = formatdate("HH:mm", var.sql_workload_policy.full_backup_time)
     }
 
-    retention_policy {
-      retention_policy_type = "LongTermRetentionPolicy"
-      retention_duration {
-        count         = var.sql_workload_policy.full_retention_days
-        duration_type = "Days"
-      }
+    retention_daily {
+      count = var.sql_workload_policy.full_retention_days
     }
   }
 
-  sub_protection_policy {
+  protection_policy {
     policy_type = "Log"
 
-    schedule_policy {
-      schedule_policy_type       = "LogSchedulePolicy"
-      schedule_frequency_in_mins = var.sql_workload_policy.log_backup_frequency_mins
+    backup {
+      frequency_in_minutes = var.sql_workload_policy.log_backup_frequency_mins
     }
 
-    retention_policy {
-      retention_policy_type = "SimpleRetentionPolicy"
-      retention_duration {
-        count         = var.sql_workload_policy.log_retention_days
-        duration_type = "Days"
-      }
+    retention_daily {
+      count = var.sql_workload_policy.log_retention_days
     }
   }
 
